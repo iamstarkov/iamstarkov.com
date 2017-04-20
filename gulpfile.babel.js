d3e0aad57bdb7aaf7b4ca81989d8ab4a0873ef16
Vladimir Starkov <iamstarkov@gmail.com>
2017-04-20 18:21:09 +0200

    fix the build

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -61,7 +61,7 @@ const getRSS = (site, list) => {
 
 gulp.task('articles-registry', () => {
   articlesList = [];
-  return gulp.src(env === 'dev' ? ['2015-*.md'] : [ '2015-*.md', '!*draft*.md' ])
+  return gulp.src(env === 'dev' ? ['201*-*.md'] : [ '201*-*.md', '!*draft*.md' ])
     .pipe(replace('https://iamstarkov.com/', '/'))
     .pipe(replace('https://iamstarkov.com', '/'))
     .pipe((() => through.obj((file, enc, cb) => {
