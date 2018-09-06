191ee5b7508fbedf67321b792e90c387b574c91f
Vladimir Starkov <iamstarkov@gmail.com>
2018-09-07 00:24:22 +0200

    fix article menu

--- components/article-menu.js
+++ components/article-menu.js
@@ -1,19 +1,21 @@
+import Link from "next/link";
+
 export const ArticleMenu = () => (
   <nav className="article-menu">
-    <a href="/">
-      Home
-    </a>
-    {" • "}
-    <a href="/talks">
-      Talks
-    </a>
-    {" • "}
-    <a href="/about">
-      About
-    </a>
-    {" • "}
+    <Link href="/" prefetch>
+      <a>Home</a>
+    </Link>
+    {" • "}
+    <Link href="/talks/" prefetch>
+      <a>Talks</a>
+    </Link>
+    {" • "}
+    <Link href="/about/" prefetch>
+      <a>About</a>
+    </Link>
+    {" • "}
     <a href="https://twitter.com/iamstarkov" target="_blank" rel="noopener">
       Follow me on Twitter
     </a>
   </nav>
-)
+);
