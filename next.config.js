3ff1569f60ee8ebc1222f4a2b93c3125d827adce
Vladimir Starkov <iamstarkov@gmail.com>
2018-10-31 10:23:38 +0100

    support for 404 page for export

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
-const withMDX = require('@zeit/next-mdx')({
+const withMDX = require("@zeit/next-mdx")({
   extension: /\.mdx?$/
 });
 
-module.exports = withMDX({
-  pageExtensions: ['js', 'mdx', 'md'],
-})
+module.exports = {
+  ...withMDX({
+    pageExtensions: ["js", "mdx", "md"]
+  }),
+  exportPathMap: async defaultPathMap => {
+    delete defaultPathMap["/404"];
+    return {
+      "/404.html": { page: "/_error" },
+      ...defaultPathMap
+    };
+  }
+};
